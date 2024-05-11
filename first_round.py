691864eac5a346955429aa587d38aa7199e0adf1
Shathrem <zorgq8@gmail.com>
2024-05-11 12:36:24 +0300

    graphs

--- first_round.py
+++ first_round.py
@@ -6,6 +6,7 @@ file = 'raw_data/OECD.SDD.NAD.SEEA,DSD_AIR_TRANSPORT@DF_AIR_TRANSPORT,1.0+all.cs
 
 data = pd.read_csv(file)
 #time period Observation value
+
 excluded_from_list = ['Time period', 'Observation value', 'OBS_STATUS', 'Observation status']
 
 data.drop(excluded_from_list, axis=1, inplace=True)
@@ -50,7 +51,7 @@ sns.countplot(data=data_top_20, x='REF_AREA', order=top_20_ref_areas)
 
 # Add title and axis labels
 plt.title('Number of Observations by Top 20 Reference Areas')
-plt.xlabel('Reference Area')
+plt.xlabel('Reference area')
 plt.ylabel('Count')
 
 plt.xticks(rotation=45)
@@ -60,7 +61,7 @@ plt.tight_layout()
 plt.savefig('graphs/observation_areas.png', dpi=300) 
 
 plt.figure(figsize=(8, 6))
-sns.countplot(data=data, x='FREQ')
+sns.countplot(data=data, x='Frequency of observation')
 
 # Add title and axis labels
 plt.title('Count of Frequency')
@@ -72,7 +73,7 @@ plt.tight_layout()
 plt.savefig('graphs/frequency.png', dpi=300) 
 
 plt.figure(figsize=(8, 6))
-sns.countplot(data=data, x='FLIGHT_TYPE')
+sns.countplot(data=data, x='Flight type')
 
 # Add title and axis labels
 plt.title('Count of Flight Types')
@@ -87,7 +88,7 @@ data['TIME_PERIOD'] = pd.to_datetime(data['TIME_PERIOD'])
 
 # Create the line plot
 plt.figure(figsize=(12, 8))
-sns.lineplot(data=data, x='TIME_PERIOD', y='REF_AREA')
+sns.lineplot(data=data, x='TIME_PERIOD', y='Reference area')
 
 # Add title and axis labels
 plt.title('Number of Observations by Reference Area Over Time')
--- first_round_excel.py
+++ first_round_excel.py
@@ -4,41 +4,5 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 file = 'raw_data/unwto-all-data-download_112023.xlsx'
 
-data_excel = pd.read_excel(file, sheet_name="Inbound Tourism-Regions")
-
-file = 'raw_data/OECD.SDD.NAD.SEEA,DSD_AIR_TRANSPORT@DF_AIR_TRANSPORT,1.0+all.csv'
-
-data_csv = pd.read_csv(file)
-excluded_from_list = ['Time period', 'Observation value', 'OBS_STATUS', 'Observation status']
-
-data_csv.drop(excluded_from_list, axis=1, inplace=True)
-
-
-columns_mapping = {
-    'REF_AREA': 'Reference area',
-    'FREQ' :'Frequency of observation',
-    'MEASURE': 'Measure',
-    'UNIT_MEASURE': 'Unit of measure',
-    'ADJUSTMENT': 'Adjustment',
-    'POLLUTANT': 'Pollutants',
-    'FLIGHT_TYPE':'Flight type',
-    'EMISSIONS_SOURCE':'Emissions source',
-    'DECIMALS':'Decimals'
-}
-
-# Iterate over the columns and perform the operations
-for code_column, name_column in columns_mapping.items():
-    # Create a reference DataFrame for the current column
-    reference_df = data_csv[[code_column, name_column]].drop_duplicates()
-    
-    # Merge the reference DataFrame with your original DataFrame
-    merged_df = pd.merge(data_csv, reference_df, on=code_column, how='left')
-    
-    # Update the original DataFrame with the merged column
-    data_csv[code_column] = data_csv[code_column].astype('category')
-
-data_csv['UNIT_MULT'] = data_csv['UNIT_MULT'].replace([0, -999], np.nan)
-
-
-combined_data = pd.concat([data_excel, data_csv])
-print(combined_data.dtypes)
+data_transport = pd.read_excel(file, sheet_name="Inbound Tourism-Transport")
+data_accomodation = pd.read_excel(file, sheet_name="Inbound Tourism-Accommodation")
